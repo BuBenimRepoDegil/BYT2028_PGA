1219785e8bd1187e67ea9c2d3bb35b6276abb3b2
BuBenimRepoDegil <fanana9b@outlook.com>
2025-05-08 14:57:57 +0300

    Create Etkinlik08.py

---
+++ Etkinlik08.py
@@ -0,0 +1,52 @@
+true = True
+false = False
+
+
+
+stupidWorkaround = true
+testClause = true
+sl = []
+nl = []
+al = []
+print("Öğrenci Kaydı")
+while stupidWorkaround:
+    wa1 = true
+    while wa1:
+        print("Sınıfınızı girin: ",end="")
+        try:
+            x = input()
+        except:
+            print("Hatalı giriş! Tekrar dene.")
+        else:
+            wa1 = false
+        if x != "bos":
+            sl.append(x)
+        else: 
+            stupidWorkaround = false
+            testClause = false
+    if testClause:
+        wa1 = true
+        while wa1:
+            print("Numaranızı girin: ",end="")
+            try:
+                x = input()
+            except:
+                print("Hatalı giriş! Tekrar dene.")
+            else:
+                wa1 = false
+            nl.append(x)
+        wa1 = true
+        while wa1:
+            print("Adınızı soyadınızı girin: ",end="")
+            try:
+                x = input()
+            except:
+                print("Hatalı giriş! Tekrar dene.")
+            else:
+                wa1 = false
+            al.append(x)
+    m="Sınıf Listesi"
+    print(len(m)*"-","\n",m,"\n",len(m)*"-","\n",sep="",end="")
+    for i in range(len(al)):
+        print(nl[i]," ",sl[i]," - ",al[i],sep="")
+        

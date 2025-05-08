def896b25f9114762a6473ad6f85915f5e8cc0fc
BuBenimRepoDegil <fanana9b@outlook.com>
2025-05-08 15:19:33 +0300

    Update Etkinlik08.cxx

--- Etkinlik08.cxx
+++ Etkinlik08.cxx
@@ -5,26 +5,29 @@ int main() {
     bool stupid = true;
     string x, y;
     int z;
-    vector<pair<int, pair<string, string>>> n;
+    vector<tuple<string, int, string>> n;
 
     setlocale(LC_ALL, "tr_TR.UTF-8");
     cout << "Öğrenci Kaydı" << endl;
 
     while (stupid) {
-        cout << "Sınıfınız:";
+        cout << "Sınıfınız: ";
         cin >> x;
         if (x == "boş") break;
-        cout << "\nNumaranız:";
+        cout << "\nNumaranız: ";
         cin >> z;
-        cout << "\nAdınız ve Soyadınız:";
+        cout << "\nAdınız ve Soyadınız: ";
         cin.ignore();
         getline(cin, y);
-        n.push_back(make_pair(z, make_pair(x, y)));
+        n.emplace_back(x, z, y);
     }
+
+
+    sort(n.begin(), n.end());
 
     cout << "-------\nSınıf Listesi\n-------\n";
     for (auto& i : n) {
-        cout << i.second.first << " - " << i.first << " " << i.second.second << "\n";
+        cout << get<0>(i) << " - " << get<1>(i) << " " << get<2>(i) << "\n";
     }
 
     return 0;
